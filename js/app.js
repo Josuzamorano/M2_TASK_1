9e22f8edd84dd2d9d8dc98bbc2dfeeb5b4a06def
Josuzamorano <114601348+Josuzamorano@users.noreply.github.com>
2022-10-18 23:43:54 -0300

    :D

---
+++ js/app.js
@@ -0,0 +1,128 @@
+import { MyData } from './model.js';
+import { DomElement } from './view.js'
+
+let detashe = "";
+
+let datita = new MyData();
+let view = new DomElement();
+let eventos;
+let deta = localStorage.getItem("detashe");
+
+main();
+
+function main() {
+    let page = window.location.pathname.split("/").pop();
+
+    if (page === "index.html") {
+        eventos = datita.get__data.events;
+    }
+    else if (page === "past-events.html") {
+        eventos = datita.get__pastEvents;
+    }
+    else if (page === "up-events.html") {
+        eventos = datita.get__upcommingEvents;
+    }
+    else if (page === "details.html") {
+        console.log(deta);
+        eventos = buscarObjetoByName(datita.get__data.events, deta.toLocaleLowerCase());
+        console.dir(eventos);
+    }
+    view.card_selection(eventos);
+}
+
+
+view.checkbox_selection(datita.get__Categoria(eventos));
+let anc =document.querySelectorAll('.btn-color'); 
+let button = document.querySelector("#button-input");
+let text = document.querySelector('#input-search')
+let checkB = document.querySelector('#check-conteiner');
+let checkboxes = document.querySelectorAll('.checkbox');
+button.addEventListener("click", search);
+checkB.addEventListener("click", checkSearch);
+text.addEventListener('keyup', logKey);
+anc.forEach(e => e.addEventListener("click", element =>{
+    detashe =  document.querySelector(`#tittleCard_${e.id.slice(-1)}`).textContent ;
+    
+    localStorage.setItem("detashe", detashe);
+    console.log(deta);
+} ) );
+
+
+
+console.dir(eventos);
+function checkSearch() {
+    let b_active = [];
+
+    checkboxes.forEach(e => {
+        if (e.checked == true) {
+            b_active.push(e.value);
+            main();
+            return;
+        }
+    });
+    console.dir(b_active);
+    if (b_active.length == 0) {
+        main();
+        return;
+    }
+    view.clearCards();
+    eventos = buscarObjetoByCategory(eventos, b_active);
+    view.card_selection(eventos);
+
+}
+
+
+
+function search() {
+    view.clearCards();
+    view.card_selection(buscarObjetoByName(eventos, formatString(text.value)));
+}
+
+
+function logKey(e) {
+    if (text.value.trim() === "") {
+        view.clearCards();
+        view.card_selection(eventos);
+    }
+    else if (e.code === 'Enter') {
+        view.clearCards();
+        view.card_selection(buscarObjetoByName(eventos, formatString(text.value)));
+    }
+}
+function formatString(tringa) {
+
+    if (tringa === '') {
+        return;
+    }
+    else if (tringa.match(/\d+/g)) {
+        alert('Los nombres de eventos no tienen numeros ' + '\uD83D\uDD14');
+        return;
+    }
+    else {
+        console.log("aca")
+        tringa = tringa.toLowerCase().trim();
+        console.log(tringa);
+        return tringa;
+    }
+}
+
+
+function buscarObjetoByName(data, tringa) {
+    return data.filter(data => data.name.toLowerCase().trim() === tringa);
+}
+
+function buscarObjetoByCategory(data, tringa) {
+    let eventosARR = [];
+    tringa.forEach(e => {
+        data.forEach(data => {
+            if (data.category === e) {
+                eventosARR.push(data);
+            }
+        });
+    });
+    return eventosARR;
+}
+
+
+
+
---
+++ js/view.js
@@ -0,0 +1,91 @@
+export class DomElement {
+    constructor() { }
+
+
+    createCard(img_src, title_text, context_text, price_card, new_id) {
+
+        let card = document.createElement('div');
+        let img = document.createElement('img');
+        let body = document.createElement('div');
+        let tittle = document.createElement('h5');
+        let text = document.createElement('p');
+        let price = document.createElement('p');
+        let anchor = document.createElement('a');
+
+        card.id = 'card_' + new_id;
+        img.id = 'imgCard_' + new_id;
+        body.id = 'bodyCard_' + new_id;
+        tittle.id = 'tittleCard_' + new_id;
+        text.id = 'textCard_' + new_id;
+        price.id = 'priceCard_' + new_id;
+        anchor.id = 'anchorCard_' + new_id;
+
+        card.className = "card shadow-sm";
+        img.className = "card-img-top";
+        body.className = "card-body";
+        tittle.className = "card-title";
+        text.className = "card-text";
+        anchor.className = "btn-color";
+
+
+        img.src = img_src;
+        tittle.textContent = title_text;
+        text.textContent = context_text;
+        price.textContent = 'Picre: ' + price_card;
+        anchor.href = '../pages/details.html';
+        anchor.textContent ='Details'
+
+        card.appendChild(img);
+        card.appendChild(body);
+        body.appendChild(tittle);
+        body.appendChild(text);
+        body.appendChild(price);
+        body.appendChild(anchor);
+
+        return card;
+
+
+    }
+
+    card_selection(data) {
+        const fragment = document.createDocumentFragment();
+        data.forEach((element, i) => {
+            fragment.append(this.createCard(element.image, element.name, element.description, element.price, i));
+        });
+        document.getElementById("card-cotainer").appendChild(fragment);
+    }
+
+    clearCards() {
+        let elemnt = document.getElementById("card-cotainer")
+        elemnt.innerHTML = "";
+    }
+
+    createCheckBox(new_id, name) {
+        let div = document.createElement('div');
+        let checkB = document.createElement('input');
+        let label = document.createElement('label');
+
+        checkB.type = "checkbox";
+        checkB.className = "checkbox";
+        checkB.value = name;
+        div.id = "divCheckbox_" + new_id;
+        checkB.id = "checkbox_" + new_id;
+        label.id = "label_" + new_id;
+        label.textContent = name;
+
+        div.appendChild(label);
+        div.appendChild(checkB);
+
+        return div;
+    }
+
+    checkbox_selection(data) {
+        const fragment = document.createDocumentFragment();
+        data.forEach((element, i) => {
+            fragment.append(this.createCheckBox(i, element));
+        });
+        document.getElementById("check-conteiner").appendChild(fragment);
+
+    }
+
+}
